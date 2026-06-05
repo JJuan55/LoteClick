548f8e7ba99d3f325ddd27c69c6f9067a9c46742
david26044 <davidcarreno767@gmail.com>
2026-06-05 05:33:55 -0500

    Mostrar clientes al abrir cartera

--- src/main/java/com/almaros/loteclick/repositories/CompradorRepository.java
+++ src/main/java/com/almaros/loteclick/repositories/CompradorRepository.java
@@ -3,6 +3,7 @@ package com.almaros.loteclick.repositories;
 import com.almaros.loteclick.models.Comprador;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -11,6 +12,8 @@ import java.util.UUID;
  */
 @Repository
 public interface CompradorRepository extends JpaRepository<Comprador, UUID> {
+
+    List<Comprador> findAllByOrderByNombreAsc();
 
     /**
      * Busca un comprador por su número de cédula.
